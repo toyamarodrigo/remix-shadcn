2b51c542280d25d3c2dae44e266b44178c609aff
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2023-12-27 23:59:53 -0300

    Initial commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -36,6 +37,7 @@ export default function Index() {
           </a>
         </li>
       </ul>
+      <Button>SHADCN BUTTON</Button>
     </div>
   );
 }
