17c75b5ef369a6a83fe5a3c375e0deb0751e8599
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2023-12-29 20:04:18 -0300

    Update ESLint configuration and Vite setup

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,3 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -8,12 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import styles from "./tailwind.css";
-
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: styles },
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import "./tailwind.css"
 
 export default function App() {
   return (
